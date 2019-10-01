b2a853ce5b8407642efe01ee9fb578a51ae6e4ac
Inzoid <inzoid21@gmail.com>
2019-10-01 10:46:18 +0700

    add slider

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,11 +1,41 @@
+
 <div class="container">
 <div class="jumbotron mt-4">
-  <h1 class="display-4">Selamat Datang di Ravel Case</h1>
+  <h2 class="display-5">Selamat Datang di Ravel Case</h2>
   <p class="lead">Kami menyediakan berbagai tipe case untuk semua merk hp</p>
   <hr class="my-4">
   <a class="btn btn-info btn-lg" href="#" role="button">Learn more</a>
 </div>
+
+<center>
+<div id="carouselExampleIndicators" class="carousel slide mt-4 mb-5" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?= BASEURL; ?>/img/Banner.png" class="d-block w-100 mb-3">
+      
+    </div>
+    <div class="carousel-item">
+      <img src="<?= BASEURL; ?>/img/Hardcase.jpg" class="d-block w-100 mb-1">
+    </div>
+    <div class="carousel-item">
+      <img src="<?= BASEURL; ?>/img/Soft.jpg" class="d-block w-100">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
+</center>
 
 <div class="container">
 <div class="card-deck">
@@ -21,8 +51,8 @@
   </div>
   <div class="card text-center">
     <img src="<?= BASEURL; ?>/img/Banner.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Premium Case</h5>
+    <div class="card-body mt-5">
+      <h5 class="card-title mb-5">Premium Case</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
     </div>
     <div class="card-footer">
@@ -41,3 +71,5 @@
   </div>
 </div>
 </div>
+
+
